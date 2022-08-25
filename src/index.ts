b5f8013a05189cecbe0e779b86d0540eb0e64dae
‘Pustaka-Digital-Media’ <corporate@pustaka.co.in>
2022-08-25 17:39:47 +0530

    chore: prisma setup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get("/", async (_: Request, res: Response) => {
+  const allBooksByAuthor = await prisma.book_tbl.findMany({
+    where: {
+      author_name: 4,
+    },
+  });
+
+  res.json(allBooksByAuthor);
 });
 
 app.listen(port, () => {
