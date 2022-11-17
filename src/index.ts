03a9fb91c42b949247b0b97aa1d94588a97cdd50
‘Pustaka-Digital-Media’ <corporate@pustaka.co.in>
2022-11-17 19:23:55 +0530

    fix(index): fixed cors error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
-import cors from "cors";
+// const cors = require("cors");
 
 import dashboardRouter from "./routes/dashboard";
 import userRouter from "./routes/user";
@@ -11,7 +11,23 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use(cors());
+
+app.use(function (req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  // Set custom headers for CORS
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-type,Accept,X-Custom-Header"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  return next();
+});
 
 app.use("/dashboard", dashboardRouter);
 app.use("/user", userRouter);
